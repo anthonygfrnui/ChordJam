8ecf40ceccb78379c4282964e298ebe05966d648
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-06-14 09:09:49 +0700

    Initial Commit

---
+++ ChordJam/ChordJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordJamApp.swift
+//  ChordJam
+//
+//  Created by Anthony on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChordJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
